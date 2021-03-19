3ca9979d1039c923c6a10eb2101d6f20f2eb0a17
Leandro-Pires001 <francisco.leandro.sousa09@aluno.ifce.edu.br>
2021-03-19 11:14:31 -0300

    atualização do crud exames

--- com.canalgeekdev.cliente/src/main/java/com/canalgeekdev/cliente/testePessoa.java
+++ com.canalgeekdev.cliente/src/main/java/com/canalgeekdev/cliente/testePessoa.java
@@ -3,40 +3,44 @@ package com.canalgeekdev.cliente;
 import java.util.List;
 
 import com.canalgeekdev.cliente.model.conecxaoPessoa;
+import com.canalgeekdev.cliente.model.Conecxao;
 import com.canalgeekdev.cliente.model.Pessoa;
 
 public class testePessoa {
 	public static void main(String[] args) {
 			//salvar
-		conecxaoPessoa conecxao = new conecxaoPessoa();
+	conecxaoPessoa conecxao = new conecxaoPessoa();
 		Pessoa pessoa = new Pessoa();
-		pessoa.setNome("Anderson Silva");
-		pessoa.setCpf("01341933013");
-		pessoa.setRg("20083401934");
+		pessoa.setNome("Felipe Galhardo");
+		pessoa.setCpf("213645258");
+		pessoa.setRg("104308294");
 		
 	 conecxao.salvarDados(pessoa);
-		
+	 
+	 	
 			//atualizar
-		//Conecxao conecxao = new Conecxao();
-		
-		//Dados dados = conecxao.getId(3);
-		// dados.setNome("João Acacil");
-		//conecxao.salvarDados(dados);
+		//conecxaoPessoa pc1 = new conecxaoPessoa();
+		//Pessoa dados = pc1.getId(3);
+		 //dados.setNome("João Acacil");
+		//pc1.salvarDados(dados);
 		
 		
 		
 			//remover
-		//Conecxao conecxao = new Conecxao();
-		//Dados dados = conecxao.getId(2);
+		/*Conecxao conecxao = new Conecxao();
+		Pessoa dados = conecxao.getId(2);
 		
-		//Conecxao conecxao1 = new Conecxao();
-		//conecxao.removerDados(dados);
+		Conecxao conecxao1 = new Conecxao();
+		conecxao.removerDados(dados);
 		
 			//lista
-		//List<Dados> dados = conecxao.listarTodos();
+		conecxaoPessoa cp = new conecxaoPessoa();
+		List<Pessoa> pessoas = cp.listarTodos();
 		
-		//for (Dados dados2 : dados) {
-			//System.out.println(dados2.getNome() + dados2.getCpf() +  dados2.getRg());
-		//}
+		for (Pessoa pessoa : pessoas) {
+			System.out.println(pessoa.getNome() + pessoa.getCpf() +  pessoa.getRg());
+		}
+			
+		*/
 	}
 }
